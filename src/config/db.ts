c2c96500854ff6467cce4d4022a4b72baf1ddd4b
Teemu Tverin <teemu.tverin@zuhlke.com>
2026-03-27 10:49:39 +0100

    feat: add psql

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,9 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT),
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
+    ssl: process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false }
+        : undefined,
 });
